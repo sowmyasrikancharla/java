c3e9c5a6c94f22c6f9ff1f8e24deca32051475ff
sowmyasrikancharla <sowmyakancharla2424@gmail.com>
2024-09-05 18:33:42 +0530

    java

---
+++ NFE.java
@@ -0,0 +1,17 @@
+
+package Exer_6;
+
+public class e_NFE {
+    public static void main(String[] args) {
+        String s = "Rohit";
+        try {
+            System.out.println(Integer.parseInt(s));
+        } catch (NumberFormatException nfe) {
+            System.out.println("Exception: Number Format Exception");
+        }
+    }
+}
+/*
+ * Output:-
+ * Exception: Number Format Exception
+ */
